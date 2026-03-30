94460a1790a05930db1f482bdcba92c37d391482
hg13-little <hector.gonzalez001>
2026-03-30 12:32:39 -0400

    code

---
+++ supermarket.py
@@ -0,0 +1,10 @@
+import random 
+
+class GroceryItem:
+    __slots__ = ("name", "base_price", "current_price")
+    
+    def __init__(self, name, price,):
+        self.name = name
+        self.base_price = price
+        self.current_price = price
+        
